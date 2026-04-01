42d91cbc394c5781dac065f0fc2288feb3bb13d4
fiscove <realfishtrust@gmail.com>
2026-03-31 21:29:00 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,8 +51,8 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
+                            <div class="type-title-sm">World Record</div>
+                            <p>{{ level.wr || 'World Record' }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">Difficulty Index</div>
